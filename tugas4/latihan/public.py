2df0707463557651b14e21a0be021adc94740ffd
Bellagemoy <bellatriana369@gmail.com>
2022-03-08 14:41:32 +0700

    YEY

--- tugas4/latihan/public.py
+++ tugas4/latihan/public.py
@@ -1,11 +1,12 @@
-class Segitiga:
-     def _init_(self, alas, tinggi):
-          self.alas = alas
-          self.tinggi = tinggi
-          self.luas = 0.5 * alas * tinggi
-segitiga_besar = Segitiga(100,80)
+class segitiga:
+    def __init__(self, alas, tinggi) -> None:
+        self.alas = alas
+        self.tinggi = tinggi
+        self.luas = 0.5 * alas * tinggi
 
-# akses variable public :alas, tinggi, dan luas dari kelas segitiga
-print("alas: ", segitiga_besar.alas)
-print("tinggi:", segitiga_besar.tinggi)
-print("luas: ", segitiga_besar.luas)
+
+hitung = segitiga(100, 80)
+
+print('Alas segitita = ', hitung.alas)
+print('Tinggi segitiga = ', hitung.tinggi)
+print('Luas segitiga = ', hitung.luas)
